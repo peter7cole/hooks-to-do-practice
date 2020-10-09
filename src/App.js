a48731b305187ae9769cf8839dac2c69e71be4d6
‘peter-robert-cole’ <petercole3@gmail.com>
2020-10-08 19:12:39 -0700

    useState and useCallback Hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+	// useState returns an array that contains the value of the state item and a function that can manipulate that state
+	// As done below, passing in and initial default value of 'Peter'
+	const [name, setName] = useState('Peter');
+	// useCallback creates a cached version of the function so it doesn't have to recreate any values on each render, no dependencies on external state
+	const onNameChange = useCallback(event => {
+		console.log(event.target.value);
+		setName(event.target.value);
+	}, []);
 	return (
 		<div className="App">
-			<h1>Hello</h1>
+			<h1>Hello, {name}!</h1>
+			<form>
+				<label>Enter your name</label>
+				<input placeholder="Name" onChange={onNameChange} />
+			</form>
 		</div>
 	);
-}
+};
 
 export default App;
